84d8a8b050fbaec353ebe084b0b6ff2337c989c8
rico157 <cogonir@live.com>
2020-08-27 16:34:37 +0100

    add logo

--- new-game.js
+++ new-game.js
@@ -1,17 +1,27 @@
 const inquirer = require("./lib/inquirer");
 const { Pokemon, Trainer, Battle, pokemons, trainers } = require("./pokemon");
-
+const terminalImage = require("terminal-image");
+(async () => {
+  console.log(
+    await terminalImage.file("./img/logo.png", { width: "50%", height: "50%" })
+  );
+})();
 const questions = [
   {
-    type: "list",
-    name: "pokemon",
-    message: "Pick a Pokeball",
-    choices: ["Pikachu", "Bulbasaur", "Charmander", "Squirtle"],
+    type: "input",
+    name: "Press Enter",
+    message: "",
   },
   {
     type: "input",
     name: "name",
     message: "Choose a name",
+  },
+  {
+    type: "list",
+    name: "pokemon",
+    message: "Pick a Pokeball",
+    choices: ["Pikachu", "Bulbasaur", "Charmander", "Squirtle"],
   },
 ];
 
@@ -27,6 +37,9 @@ const battleQ = [
 inquirer.prompt(questions).then((answers) => {
   trainers.one = new Trainer(answers.name);
   trainers.one.catch(pokemons[answers.pokemon.toLowerCase()]);
+  // (async () => {
+  //   console.log(await terminalImage.file("./img/logo.png"));
+  // })();
   // Battle
   const battle = new Battle(trainers.one, trainers.two);
   console.log(
